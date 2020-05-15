78465c9177401934203ef8bfc0a98f54772625a5
marcos <marcosrodriguesbrugnaro@gmail.com>
2020-05-15 06:21:40 -0300

    Get transactions and balance.

---
+++ readme.md
@@ -0,0 +1,2 @@
+https://www.youtube.com/watch?v=FYsFvjM6AJM&feature=youtu.be
+33:02
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
-import TransactionsRepository from '../repositories/TransactionsRepository';
-import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import CreateTransactionService from '../services/CreateTransactionService'
 
 const transactionRouter = Router();
 
@@ -9,7 +9,13 @@ const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const ballance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      ballance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -20,13 +26,13 @@ transactionRouter.post('/', (request, response) => {
     const { title, value, type } = request.body;
 
     const createTransaction = new CreateTransactionService(
-      transactionsRepository
+      transactionsRepository,
     );
 
     const transaction = createTransaction.execute({
       title,
       value,
-      type
+      type,
     });
 
     return response.json(transaction);
